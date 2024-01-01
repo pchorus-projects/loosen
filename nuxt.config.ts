a4883ad3d3411d4967335708e6b3235f00d5b3aa
Pascal Chorus <p.chorus@me.com>
2024-01-01 19:35:56 +0100

    feat(app): add sitemap generator

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/image'],
+  modules: ['@nuxt/image', 'nuxt-simple-sitemap'],
   css: ['@/assets/styles/styles.scss'],
   devtools: { enabled: true },
   image: {
     provider: 'ipx',
+  },
+  site: {
+    url: 'https://loosen.ac',
   },
 });
